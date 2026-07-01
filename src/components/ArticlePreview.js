8ae6bd8b9e3dac2da19d37552b0da1722c7a6d7b
joyrahaASC <joy.raha@ascendion.com>
2026-07-01 11:31:40 -0500

    Auto-generated RCA fix by Deep Level Agent

--- src/components/ArticlePreview.js
+++ src/components/ArticlePreview.js
@@ -18,62 +18,110 @@ const mapDispatchToProps = dispatch => ({
   })
 });
 
-const ArticlePreview = props => {
-  const article = props.article;
-  const favoriteButtonClass = article.favorited ?
-    FAVORITED_CLASS :
-    NOT_FAVORITED_CLASS;
+class ArticlePreview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      pending: false
+    };
+  }
 
-  const handleClick = ev => {
+  handleClick = ev => {
     ev.preventDefault();
-    if (article.favorited) {
-      props.unfavorite(article.slug);
-    } else {
-      props.favorite(article.slug);
+
+    const { article } = this.props;
+
+    if (this.state.pending) {
+      return;
     }
+
+    this.setState({ error: null, pending: true });
+
+    const action = article.favorited ?
+      this.props.unfavorite(article.slug) :
+      this.props.favorite(article.slug);
+
+    return Promise.resolve(action)
+      .then(() => {
+        this.setState({ pending: false, error: null });
+      })
+      .catch(error => {
+        let errorMessage = 'Unable to update favorite. Please try again.';
+
+        if (error && error.response && error.response.body && error.response.body.errors) {
+          const errors = error.response.body.errors;
+          errorMessage = Object.keys(errors)
+            .map(key => `${key} ${errors[key].join(', ')}`)
+            .join(', ');
+        } else if (error && error.message) {
+          errorMessage = error.message;
+        }
+
+        this.setState({
+          pending: false,
+          error: errorMessage
+        });
+      });
   };
 
-  return (
-    <div className="article-preview">
-      <div className="article-meta">
-        <Link to={`/@${article.author.username}`}>
-          <img src={article.author.image} alt={article.author.username} />
-        </Link>
+  render() {
+    const article = this.props.article;
+    const favoriteButtonClass = article.favorited ?
+      FAVORITED_CLASS :
+      NOT_FAVORITED_CLASS;
 
-        <div className="info">
-          <Link className="author" to={`/@${article.author.username}`}>
-            {article.author.username}
+    return (
+      <div className="article-preview">
+        <div className="article-meta">
+          <Link to={`/@${article.author.username}`}>
+            <img src={article.author.image} alt={article.author.username} />
           </Link>
-          <span className="date">
-            {new Date(article.createdAt).toDateString()}
-          </span>
+
+          <div className="info">
+            <Link className="author" to={`/@${article.author.username}`}>
+              {article.author.username}
+            </Link>
+            <span className="date">
+              {new Date(article.createdAt).toDateString()}
+            </span>
+          </div>
+
+          <div className="pull-xs-right">
+            <button
+              className={favoriteButtonClass}
+              onClick={this.handleClick}
+              disabled={this.state.pending}>
+              <i className="ion-heart"></i> {article.favoritesCount}
+            </button>
+          </div>
         </div>
 
-        <div className="pull-xs-right">
-          <button className={buttonClass} onClick={testingClick}>
-            <i className="ion-heart"></i> {article.favoritesCount}
-          </button>
-        </div>
+        {this.state.error ? (
+          <div className="article-preview-error">
+            {this.state.error}
+          </div>
+        ) : null}
+
+        <Link to={`/article/${article.slug}`} className="preview-link">
+          <h1>{article.title}</h1>
+          <p>{article.description}</p>
+          <span>Read more...</span>
+          <ul className="tag-list">
+            {
+              article.tagList.map(tag => {
+                return (
+                  <li className="tag-default tag-pill tag-outline" key={tag}>
+                    {tag}
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </Link>
       </div>
-
-      <Link to={`/article/${article.slug}`} className="preview-link">
-        <h1>{article.title}</h1>
-        <p>{article.description}</p>
-        <span>Read more...</span>
-        <ul className="tag-list">
-          {
-            article.tagList.map(tag => {
-              return (
-                <li className="tag-default tag-pill tag-outline" key={tag}>
-                  {tag}
-                </li>
-              )
-            })
-          }
-        </ul>
-      </Link>
-    </div>
-  );
+    );
+  }
 }
 
 export default connect(() => ({}), mapDispatchToProps)(ArticlePreview);
